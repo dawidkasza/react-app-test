7b020547d44ae1e35f2ac59c9c05ebf366b28f3e
Dawid Kasza <dawidkasza05@gmail.com>
2025-12-05 21:02:32 +0100

    Add search feature

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,7 +4,9 @@ import CardForm from '../CardForm/CardForm';
 import styles from './Column.module.scss';
 
 const Column = props => {
-   const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+   const searchString = useSelector(state => state.searchString);
+   const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id && card.title.toLowerCase().includes(searchString.toLowerCase())));
+   console.log("searchString =", searchString, typeof searchString);
    return (
       <article className={styles.column}>
          <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,19 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 const SearchForm = () => {
+    const [searchString, setSearchString] = useState('');
+    const dispatch = useDispatch();
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: 'UPDATE_SEARCHSTRING', payload: searchString });
+    }
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search"/>
+        <form onSubmit={handleSubmit} className={styles.searchForm}>
+            <TextInput placeholder="Search" value={searchString} onChange={e => setSearchString(e.target.value)}/>
             <Button><span className="fa fa-search" /></Button> 
         </form>
     );
